2376073a880dc8516f0e2981ebf21a4055f3c719
Iacobucci Juan B. <link00222@gmail.com>
2025-06-29 17:16:45 -0300

    euler 001

---
+++ euler/001/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int sum_of_multiples(int limit) {
+    int total_sum = 0;
+
+    for (int x = 0; x < limit; ++x) {
+        if (x % 3 == 0 || x % 5 == 0) {
+            total_sum += x;
+        }
+    }
+
+    return total_sum;
+}
+
+int main() {
+    int result = sum_of_multiples(1000);
+    std::cout << result << std::endl;
+    return 0;
+}
